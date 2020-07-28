4febb58995fa0849a0fe919d768ec786d4edf0e2
xgreenspace <amariawest@gmail.com>
2020-07-28 16:22:30 -0500

    Added .gitignore

--- WebScraper2.py
+++ WebScraper2.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 import os
 import sys
 import time
@@ -11,8 +12,22 @@ class TalentSearch():
     def __init__(self, job='', username='', password=''):
 
         # Initialize chromedriver and creates username and password
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome('chromedriver/chromedriver.exe')
 
+        self.base = 'https://www.linkedin.com'
+        self.login = self.base + '/login'
+        self.feed = self.base + '/feed'
+        self.search_people = self.base + '/search/results/all/?keywords='
+
+        # Signs into LinkedIn with given creditentials  
+
+        self.username = username
+        self.password = password
+
+        self.driver.get(self.login)
+        self.driver.find_element_by_id('username').send_keys(self.username)
+        self.driver.find_element_by_id('password').send_keys(self.password)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Sign in')]").click()
 
         # Gets web content from a linkedIn List pretaining to a specific job search.
         
@@ -20,4 +35,4 @@ class TalentSearch():
         # Accesses the website and assigns the data to a variable
         
 
-app1 = TalentSearch(2)
+app1 = TalentSearch(username='amariawest@gmail.com', password='Tealsoap35')
