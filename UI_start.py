09189d46160ac4cf994c5000fa4dd97f9a270f4c
Rolando Vera <verarolandojr76@gmail.com>
2020-07-28 16:39:52 -0500

    changes

--- UI_start.py
+++ UI_start.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/home", methods=["POST"])
+def home():
+    J = request.form.get("Job")
     return render_template("home.html")
