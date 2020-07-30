0f8696b38091ab9768deff502d7626774c2f76e0
xgreenspace <amariawest@gmail.com>
2020-07-29 21:28:13 -0500

    Added analyze class

--- analyzing.py
+++ analyzing.py
@@ -1,8 +1,39 @@
+from candidates import *
+
 
 class Analyze():
     def __init__(self, candidate=-1):
         if candidate == -1:
             raise TypeError('Nothing was passed.')
 
-        open 
+        key_words = self.get_key_words()
+
+        for word in key_words:
+            if word in candidate.summary:
+                candidate.merit_points += 1
+
+        if candidate.merit_points >= 30:
+            candidate.is_good_hire = True
+        else:
+            pass
         
+
+    def get_key_words(self):
+        key_words = []
+        i = 0
+        with open('resume-words.text', 'r') as file:
+            while True:
+                if not file.readline():
+                    break
+                i += 1
+                key_words.append(file.readline())
+
+
+        
+        for i in range(len(key_words)):
+            key_words[i] = key_words[i][:key_words[i].find('\n')]
+
+        key_words.pop(-1)
+        
+        return(key_words)
+
--- candidates.py
+++ candidates.py
@@ -1,7 +1,11 @@
 class Candidate():
     def __init__(self, name='', profile_link='', summary='', current_role=''):
+        self.merit_points = 0
+        self.is_good_hire = False
 
         self.name = name
         self.profile_link = profile_link
         self.summary = summary
         self.current_role = current_role
+
+        
